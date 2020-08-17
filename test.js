fb2036102a54d1fdb92ac7e1d292d8a9782263df
Camezza <42525540+Camezza@users.noreply.github.com>
2020-08-18 09:24:06 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,32 @@
-let x_value = 2, y_value = 5;
-let x2_value = 18, y2_value = 27;
+let x_value = 10, y_value = 10;
+let x2_value = 15, y2_value = 15;
 
-let x_offset = x_value + x2_value;
-let y_offset = y_value + y2_value;
+let x_offset = x2_value - x_value
+let y_offset = y2_value - y_value;
+
+// Diagonal
+if (Math.abs(x_offset) === Math.abs(y_offset)) {
+    let x = 0;
+    let y = 0;
 
 
-for (let x = 0; x_offset > 0 ? (x < Math.abs(x_offset) + 1) : (x >= x_offset); x_offset > 0 ? x++ : x--) {
-
-    for (let y = 0; y_offset > 0 ? (y < y_offset + 1) : (y >= y_offset); y_offset > 0 ? y++ : y--) {
-        console.log([x, y]);
-    }
+    throw ("Diagonal number");
 }
+
+// Longitudinal
+else if (x_offset === 0 || y_offset === 0) {
+    // X loop
+    for (let x = 0; x_offset > 0 ? (x < x_offset + 1) : (x >= x_offset); x_offset > 0 ? x++ : x--) {
+
+        // Y loop
+        for (let y = 0; y_offset > 0 ? (y < y_offset + 1) : (y >= y_offset); y_offset > 0 ? y++ : y--) {
+            console.log([x, y]); 
+            // Prevent using 0, 0
+        }
+    }
+
+    throw ("Longitudinal number");
+}
+
+else throw ("Number not even");
+
