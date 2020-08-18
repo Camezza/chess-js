d31770fef4653b2ab86ccd9b1aafd029003daf3e
Camezza <42525540+Camezza@users.noreply.github.com>
2020-08-18 11:01:47 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 let x_value = 10, y_value = 10;
-let x2_value = 15, y2_value = 15;
+let x2_value = 5, y2_value = 10;
 
 let x_offset = x2_value - x_value
 let y_offset = y2_value - y_value;
 
 // Diagonal
 if (Math.abs(x_offset) === Math.abs(y_offset)) {
-    let x = 0;
-    let y = 0;
+
+    // X and Y loop both increase by 1/-1
+    for (let x = 0, y = 0; (x_offset > 0 ? (x < x_offset) : (x > x_offset) && y_offset > 0 ? (y < y_offset) : (y > y_offset));) {
+        
+        // Prevents starting from 0, 0 
+        let x_counter_offset = x_offset > 0 ? 1 : -1;
+        let y_counter_offset = y_offset > 0 ? 1 : -1;
+
+        console.log([x + x_counter_offset, y + y_counter_offset]);
+
+        // Increment / Decrement
+        x_offset > 0 ? x++ : x--;
+        y_offset > 0 ? y++ : y--;
+    }
+
 
 
     throw ("Diagonal number");
@@ -16,12 +29,13 @@ if (Math.abs(x_offset) === Math.abs(y_offset)) {
 // Longitudinal
 else if (x_offset === 0 || y_offset === 0) {
     // X loop
-    for (let x = 0; x_offset > 0 ? (x < x_offset + 1) : (x >= x_offset); x_offset > 0 ? x++ : x--) {
+    for (let x = 0; x_offset > 0 ? (x < x_offset) : (x >= x_offset); x_offset > 0 ? x++ : x--) {
 
         // Y loop
-        for (let y = 0; y_offset > 0 ? (y < y_offset + 1) : (y >= y_offset); y_offset > 0 ? y++ : y--) {
-            console.log([x, y]); 
-            // Prevent using 0, 0
+        for (let y = 0; y_offset > 0 ? (y < y_offset) : (y >= y_offset); y_offset > 0 ? y++ : y--) {
+            let x_counter_offset = x_offset === 0 ? 0 : (x_offset > 0 ? 1 : -1);
+            let y_counter_offset = y_offset === 0 ? 0 : (y_offset > 0 ? 1 : -1);
+            console.log([x + x_counter_offset, y + y_counter_offset]); 
         }
     }
 
