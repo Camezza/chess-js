1b353872375371404803978f18b17f379f571a07
Camezza <42525540+Camezza@users.noreply.github.com>
2020-09-03 09:28:39 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,48 +1,55 @@
-let x_value = 0, y_value = 2;
-let x2_value = 0, y2_value = 2;
+function getInfinitePieceMoves(board, piece, offset, maximum_take_threshold, applying_check) {
+    // Iteration
+    let movePath = [];
+    let piece_counter = 0;
 
-let x_offset = x2_value - x_value
-let y_offset = y2_value - y_value;
+    // Coordinates
+    let x_offset = offset[0];
+    let y_offset = offset[1];
+    let coords = addVector(board, piece.location, [x_offset, y_offset]);
+    let square = coords === null ? {} : getSquare(board, coords);
 
-// Diagonal
-if (Math.abs(x_offset) === Math.abs(y_offset)) {
+    // Misc.
+    let colour = square.occupation !== undefined && square.occupation !== null ? square.occupation.colour : piece.colour;
+    let takeable = isWhite(colour) === !isWhite(piece.colour);
 
-    x_offset = x_offset === 0 ? 0 : (x_offset > 0 ? x_offset - 1 : x_offset + 1);
-    y_offset = y_offset === 0 ? 0 : (y_offset > 0 ? y_offset - 1 : y_offset + 1);
+    // Square empty, or taking a piece
+    while ((square.occupation === null || takeable || applying_check) && piece_counter < maximum_take_threshold) {
 
-    // X and Y loop both increase by 1/-1
-    for (let x = 0, y = 0; (x_offset > 0 ? (x < x_offset) : (x > x_offset) && y_offset > 0 ? (y < y_offset) : (y > y_offset));) {
+        // Apply increments/decrements and add the new position
+        movePath.push([x_offset, y_offset]);
 
-        // Prevents starting from 0, 0 
-        let x_counter_offset = x_offset > 0 ? 1 : -1;
-        let y_counter_offset = y_offset > 0 ? 1 : -1;
-
-        console.log([x + x_counter_offset, y + y_counter_offset]);
-
-        // Increment / Decrement
-        x_offset > 0 ? x++ : x--;
-        y_offset > 0 ? y++ : y--;
-    }
-
-    throw ("Diagonal number");
-}
-
-// Longitudinal
-else if (x_offset === 0 || y_offset === 0) {
-    
-    // X loop
-    for (let x = 0; x === 0 || (x_offset > 0 ? (x < x_offset - 1) : (x > x_offset + 1)); x_offset > 0 ? x++ : x--) {
-
-        // Y loop
-        for (let y = 0; y === 0 || (y_offset > 0 ? (y < y_offset - 1) : (y > y_offset + 1)); y_offset > 0 ? y++ : y--) {
-            let x_counter_offset = x_offset === 0 ? 0 : (x_offset > 0 ? 1 : -1);
-            let y_counter_offset = y_offset === 0 ? 0 : (y_offset > 0 ? 1 : -1);
-            console.log([x + x_counter_offset, y + y_counter_offset]);
+        // Diagonal. (Bishops, etc.)
+        if (Math.abs(x_offset) === Math.abs(y_offset)) {
+            x_offset > 0 ? x_offset++ : x_offset--;
+            y_offset > 0 ? y_offset++ : y_offset--;
         }
+
+        // Longitudinal. (Rooks, etc.)
+        else if (x_offset === 0 || y_offset === 0) {
+            // Y movement
+            if (x_offset === 0) {
+                y_offset > 0 ? y_offset++ : y_offset--;
+            }
+
+            // X movement
+            else if (y_offset === 0) {
+                x_offset > 0 ? x_offset++ : x_offset--;
+            }
+        }
+        else return null;
+
+        // Piece can be taken
+        if (takeable || applying_check) {
+            piece_counter++;
+        }
+
+        // Update values
+        coords = addVector(board, piece.location, [x_offset, y_offset]);
+        square = coords === null ? {} : getSquare(board, coords);
+        colour = square.occupation !== undefined && square.occupation !== null ? square.occupation.colour : piece.colour;
+        takeable = isWhite(colour) === !isWhite(piece.colour);
+
     }
-
-    throw ("Longitudinal number");
+    return movePath;
 }
-
-else throw ("Number not even");
-
